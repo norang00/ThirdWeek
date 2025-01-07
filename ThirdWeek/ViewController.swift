4cd8876680321815857c443fa55b163c864b8f38
norang <norang1221@gmail.com>
2025-01-07 11:17:25 +0900

    Initial Commit

---
+++ ThirdWeek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThirdWeek
+//
+//  Created by Kyuhee hong on 1/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
